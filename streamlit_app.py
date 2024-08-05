339f1bbffcf50051f662f6558c23a387fde46457
IsaacMather <idmather@ucdavis.edu>
2024-08-05 11:41:54 -0700

    troubleshooting streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,13 @@
 # Import python packages
 import streamlit as st
-# from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie  :cup_with_straw:")
 st.write("Choose the fruits you want in your custom Smoothie!")
 
-
-from snowflake.snowpark.functions import col
-
-cnx=st.connection("snowflake")
-session = cnx.session
-
+cnx = st.connection("snowflake")
+session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data = my_dataframe, use_container_width=True)
 
@@ -45,6 +40,3 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
-
-        
-
