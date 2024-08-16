2dc155bc71a32211a7dc834f00db71f4e34c46ec
IsaacMather <idmather@ucdavis.edu>
2024-08-16 16:42:39 -0700

    Course Lesson 10: Using API Data With Variables 🥋 Updating the multiselect (Part 2) ▪️

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie  :cup_with_straw:")
@@ -14,8 +15,16 @@ st.write("Choose the fruits you want in your custom Smoothie!")
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data = my_dataframe, use_container_width=True)
+# st.dataframe(data = my_dataframe, use_container_width=True)
+# st.stop()
+
+
+#convert the snowpark dataframe to a pandas dataframe so we can use the LOC function.
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
+
+
 
 name_on_order = st.text_input('Name on Smoothie:')
 st.write("The name on your smoothie will be: " + name_on_order)
